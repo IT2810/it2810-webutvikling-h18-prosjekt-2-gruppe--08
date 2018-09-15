04acb79336c0b32399184c042582eb4d03fa548c
Eirik Dahlen <dahleneirik@gmail.com>
2018-09-15 17:51:03 +0200

    #12 prøvd litt ajax-fetching med componentDidMount

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 
 
+const URL_HOME = 'http://localhost:3004/home'
+
 class Home extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            home:''
+        }
+    }
+
+    componentDidMount(){
+        fetch(URL_HOME, {method:'GET'})
+        .then(response => response.json())
+        .then(json =>{
+            this.setState({
+                home:json
+            })
+        })
+    }
 
     render() {
         return(
             <div className="home_container">
-                <div className="picture">
-                    BILDE ghjdklv
+                <div className="picture" >
+                    BILDE
                 </div>
                 <div className="text">
                     TEXT agvdkjlø
